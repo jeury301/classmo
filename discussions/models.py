319b66e32b83cccff69b9bbbc046baaac0b13391
Michael Anderson <mganderson87@gmail.com>
2018-02-17 13:25:01 -0500

    Fix Comment

--- discussions/models.py
+++ discussions/models.py
@@ -4,11 +4,13 @@ from schedules.models import BaseModel, Subject
 # Create your models here.
 class Comment(BaseModel):
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
-    post = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+    post = models.ForeignKey('self', related_name='top_level_post', 
+                             on_delete=models.CASCADE, blank=True, null=True)
+    parent = models.ForeignKey('self', related_name='parent_comment',
+                                on_delete=models.CASCADE, blank=True, null=True)
     active = models.BooleanField(default=True)
-    body = models.TextField(max_length=3000)
+    body = models.TextField(max_length=3000, default="")
     score = models.IntegerField(default=1)
     
-
-
+    def __str__(self):
+        return self.body
