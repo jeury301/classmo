501e2b8fc619e20228c706dcbe19e363ec55ee25
Michael Anderson <mganderson87@gmail.com>
2018-02-17 13:10:44 -0500

    Add BaseModel and modify Comment

--- discussions/models.py
+++ discussions/models.py
@@ -1,7 +1,14 @@
 from django.db import models
-from schedules.models import Subject 
+from schedules.models import BaseModel, Subject 
 
 # Create your models here.
-class Comment(models.Model):
-    question_text = models.CharField(max_length=200)
-    class_id = models.ForeignKey(Subject, on_delete=models.CASCADE)
+class Comment(BaseModel):
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    post = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+    active = models.BooleanField(default=True)
+	body = models.TextField(max_length=3000)
+	score = models.IntegerField(default=1)
+	
+
+
--- schedules/models.py
+++ schedules/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
+# Base model for others to inherit from
+class BaseModel(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
 # Create your models here.
-class Subject(models.Model):
+class Subject(BaseModel):
     name = models.CharField(max_length=200)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
