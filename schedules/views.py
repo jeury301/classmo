6ef16c125c7ed089f293946040d239b26c2b76fa
cs491 <cs491@michaels-mbp-5.resource.campus.njit.edu>
2018-02-17 15:20:25 -0500

    Added the login shell and user login validation

--- schedules/views.py
+++ schedules/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
 from django.http import HttpResponse
-
+from django.urls import reverse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the schedules index.")
+	##user=request.user gets user object
+	return render(request,'schedules/index.html')
+
+def new_user(request):
+	username=request.POST.get('user','')
+	password=request.POST.get('password','')
+	user=User.objects.create_user(username,'invadeyou67@email.com',password)
+	user.first_name='Attila'
+	user.last_name="The hun"
+	user.save()
+	return HttpResponseRedirect(reverse('schedules:index'))
