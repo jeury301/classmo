0e46fe06fd0d1c937600dd57204d98dc14255155
cs491 <cs491@michaels-mbp-5.resource.campus.njit.edu>
2018-02-22 18:11:31 -0500

    Final changes for the ui and asingments, removed instructor from the assignment model

--- schedules/views/sessions.py
+++ schedules/views/sessions.py
@@ -7,6 +7,9 @@ from django.urls import reverse
 from schedules.models import Session, Registration, Subject, User, Assignment
 
 def sessions(request,session_id):
+	"""
+    Get's a session, this controller is probably redundent at this point. No html or view leads to this view
+    """
 	try:
 		sesh = Session.objects.get(pk=session_id)
 	except Session.DoesNotExist:
@@ -14,7 +17,11 @@ def sessions(request,session_id):
 	reg_list=sesh.registration_set.all()
 	return render(request,'schedules/sessions/session.html',{'session':sesh,'reg_list':reg_list})
 
+@login_required
 def registration(request,session_id):
+	"""
+	Process's a registration, given user.pk as post, login is required. Comes from subject/session.html
+	"""
 	sesh=get_object_or_404(Session,pk=session_id)
 	user_id=request.POST.get("user_id")
 	user_id=int(user_id)
@@ -30,11 +37,15 @@ def registration(request,session_id):
 
 @login_required
 def assignments(request):
+	"""
+	Gets all the user's HW assignments. They assignments are associated to a session, 
+	which is associated to a user by a registration. Comes from schedules/users/index.html
+	"""
 	user=request.user
-	session_list=user.registration_set.all()
+	reg_list=user.registration_set.all()
 	ass_list=[]
-	for sesh in session_list:
-		ass_list+=sesh.session.assignment_set.all() #this works???
+	for reg in reg_list:
+		ass_list+=reg.session.assignment_set.all() #this works???
 
 	return render(request,'schedules/sessions/assignments.html',{'assignments':ass_list,'user':user})
 
