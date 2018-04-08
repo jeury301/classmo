f7d136f633196371b67d8f00a51e11d7c72fb910
cs491 <cs491@michaels-mbp-5.resource.campus.njit.edu>
2018-04-08 13:53:39 -0400

    Added the config template tags to index and the login pages

--- schedules/context_processors.py
+++ schedules/context_processors.py
@@ -21,7 +21,19 @@ def global_context(request):
         role = "staff"
 
     # custom configurations
-    config = {}
+    config = {
+    "company": "Yoga boga",
+        "primary_color": "417690",
+        "secondary_color": "79aec8",
+        "logo": "",
+        "slogan": "Strech",
+        "font-family": '"Roboto","Lucida Grande","DejaVu Sans","Bitstream Vera Sans",Verdana,Arial,sans-serif',
+        "welcome_title": "Welcome to Classmo",
+        "welcome_body": "Classmo is the place to succeed, click below to get registered!",
+        "all_courses_body": "Here you can find all the courses available for regisration.  Click \"More Info\" to see sessions available for registration",
+        "my_courses_body": "These are courses that you've registered for.  You can check your existing and past registrations, and register for new sessions.",
+        "discussion_body": "Here you can ask questions and get answers for a variety of subjects"
+    }
 
     # These are the default colors / language rendedred
     # by Django's template engine when there isn't a
