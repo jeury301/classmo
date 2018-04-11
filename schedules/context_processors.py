a1309f5e5cc3fb5c064d45ad457458456d7a6056
cs491 <cs491@michaels-mbp-5.resource.campus.njit.edu>
2018-04-11 13:16:30 -0400

    added first stages of multiple image selection for splash as well as custom coloring

--- schedules/context_processors.py
+++ schedules/context_processors.py
@@ -21,27 +21,21 @@ def global_context(request):
         role = "staff"
 
     # custom configurations
-    config = {
-    "company": "Yoga boga",
-        "primary_color": "417690",
-        "secondary_color": "79aec8",
-        "logo": "",
-        "slogan": "Strech",
-        "font-family": '"Roboto","Lucida Grande","DejaVu Sans","Bitstream Vera Sans",Verdana,Arial,sans-serif',
-        "welcome_title": "Welcome to Classmo",
-        "welcome_body": "Classmo is the place to succeed, click below to get registered!",
-        "all_courses_body": "Here you can find all the courses available for regisration.  Click \"More Info\" to see sessions available for registration",
-        "my_courses_body": "These are courses that you've registered for.  You can check your existing and past registrations, and register for new sessions.",
-        "discussion_body": "Here you can ask questions and get answers for a variety of subjects"
-    }
+    
+    config = { 
+        }
+    """"
+    
+    """
+    
 
     # These are the default colors / language rendedred
     # by Django's template engine when there isn't a
     # corresponding value in the 'config' dictionary
     default_config = {
         "company": "Classmo",
-        "primary_color": "417690",
-        "secondary_color": "79aec8",
+        "primary_color": "#417690",
+        "secondary_color": "#79aec8",
         "logo": "",
         "slogan": "Organize. Connect. Achieve.",
         "font-family": '"Roboto","Lucida Grande","DejaVu Sans","Bitstream Vera Sans",Verdana,Arial,sans-serif',
@@ -49,7 +43,14 @@ def global_context(request):
         "welcome_body": "Classmo is the place to succeed, click below to get registered!",
         "all_courses_body": "Here you can find all the courses available for regisration.  Click \"More Info\" to see sessions available for registration",
         "my_courses_body": "These are courses that you've registered for.  You can check your existing and past registrations, and register for new sessions.",
-        "discussion_body": "Here you can ask questions and get answers for a variety of subjects"
+        "discussion_body": "Here you can ask questions and get answers for a variety of subjects",
+        "footer_text_color": "#f5dd5d",
+        "header_text_color": "#ffffff",
+        "splash_images": {
+        "images/splash_background1.jpg":"Photo by DAVID ILIFF. License: CC-BY-SA 3.0",
+        "images/splash_background3.jpg":"Photo by Emgonzalez License: Public Domain",
+        "images/splash_background2.jpg":"Photo by DAVID ILIFF. License: CC-BY-SA 3.0"
+        }
     }
 
     global_context = {
---
+++ schedules/templatetags/splash_images.py
@@ -0,0 +1,40 @@
+from django import template
+
+register = template.Library() 
+
+@register.filter
+def get_credit(dictionary,key):
+	return dictionary.get(key)
+
+@register.filter
+def get_transition_info(dictionary):
+	"""
+	n=number of images
+    a=image visible duration in seconds a=8
+    b=image crossfade duration in seconds b=2
+    t=total animation duration =(a+b)*n =30, result of a b and n
+	"""
+	a=8
+	b=2
+	n=0
+	for image in dictionary:
+		n+=1
+	print(a,b,n)
+	t=(a+b)*n
+	l=t
+	"""
+	oplist getsvalues based on a, b and t, the ordering matters so it should not be changed
+	"""
+	oplist=[round((a/t)*100,2),round(((a+b)/t)*100,2),round(100-(b/t*100),2)]
+	image_of_nth=[]
+	for i in range(0,n):
+		l=l-(a+b)
+		image_of_nth.append(l)
+
+	
+	trans_info={"a":a,"b":b,"n":n,"t":t,"oplist":oplist,"image_of_nth":image_of_nth}
+	return trans_info
+
+
+
+		
